43323386f95c8b801f441e30ab23ab05cb6fe453
PaulMacLachlan <paulmacother@gmail.com>
2017-03-10 21:04:58 -0800

    updates

---
+++ assignments/bank_account/mathtest.rb
@@ -0,0 +1,13 @@
+
+
+account_type = "Checking"
+# # def create_account_number (account_type)
+#     account_number = rand(999999)
+#     puts "Your #{account_type} account number is: " + account_number.to_s
+# # end
+puts "Creating a #{account_type} account!"
+account_number = rand(999999)
+puts "Your #{account_type} account number is: " + account_number.to_s
+# balance = rand.round(2)
+# puts balance
+puts "completed creating a #{account_type} account!"
