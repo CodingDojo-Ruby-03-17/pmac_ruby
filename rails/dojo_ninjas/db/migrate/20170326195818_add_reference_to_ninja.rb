3c923ddcfd551c2e4b1eb225d263f9211c551dd8
PaulMacLachlan <paulmacother@gmail.com>
2017-03-26 14:14:24 -0700

    updating

---
+++ rails/dojo_ninjas/db/migrate/20170326195818_add_reference_to_ninja.rb
@@ -0,0 +1,5 @@
+class AddReferenceToNinja < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :ninjas, :dojo, foreign_key: true
+  end
+end
--- rails/dojo_ninjas/db/schema.rb
+++ rails/dojo_ninjas/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316200605) do
+ActiveRecord::Schema.define(version: 20170326195818) do
 
   create_table "dojos", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170316200605) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "dojo_id"
+    t.index ["dojo_id"], name: "index_ninjas_on_dojo_id"
   end
 
 end
